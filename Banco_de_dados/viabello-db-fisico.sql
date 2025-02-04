ad3825a1737b297effd7a005d5a7f19336acb41a
Clean Costa <cleanrcosta@gmail.com>
2025-02-04 15:33:50 -0300

    Atualizacao Banco de dados; NavBar

--- Banco_de_dados/viabello-db-fisico.sql
+++ Banco_de_dados/viabello-db-fisico.sql
@@ -4,27 +4,32 @@
 
 
 CREATE TABLE Cliente (
-Nome VARCHAR(50),
-Email VARCHAR(50),
-Telefone VARCHAR(15),
-CPF VARCHAR(14),
-ID_Cliente (PK) INTEGER PRIMARY KEY
+Nome VARCHAR(10),
+Email VARCHAR(10),
+Telefone NUMERIC(10),
+CPF NUMERIC(10),
+ID_Cliente (PK) NUMERIC(10) PRIMARY KEY,
+senha VARCHAR(20)
 )
 
 CREATE TABLE Reserva (
-qtd_pessoas INTEGER,
-ID_Reserva (PK) INTEGER PRIMARY KEY,
+qtd_pessoas NUMERIC(10),
+ID_Reserva (PK) NUMERIC(10) PRIMARY KEY,
 data_reserva DATETIME,
-ID_Cliente (PK) INTEGER,
-ID_Destino INTEGER,
+Valor_total VARCHAR(10),
+ID_Cliente (PK) NUMERIC(10),
+ID_Destino NUMERIC(10),
 FOREIGN KEY(ID_Cliente (PK)) REFERENCES Cliente (ID_Cliente (PK))
 )
 
 CREATE TABLE Destino (
-ID_Destino INTEGER PRIMARY KEY,
+ID_Destino NUMERIC(10) PRIMARY KEY,
 Pais VARCHAR(10),
 Nome VARCHAR(10),
-Preco VARCHAR(10)
+Preco NUMERIC(10),
+Tipo VARCHAR(10),
+preco_promocao VARCHAR(10),
+Promocao VARCHAR(10)
 )
 
 ALTER TABLE Reserva ADD FOREIGN KEY(ID_Destino) REFERENCES Destino (ID_Destino)
